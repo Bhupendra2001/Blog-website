e2e7eeaf903694cf6709fc3734e57deba7cd297c
Bhupendra2001 <bhupendran42@gmail.com>
2023-03-08 11:35:25 +0530

    done

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2')
 
 
 const db = mysql.createPool({
-    host : "localhost",
+    host : "127.0.0.1",
     user : "root",
     password : "Sahil@123",
     database : "blog"
