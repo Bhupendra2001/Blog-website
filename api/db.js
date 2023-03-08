353297a274f77c01a2b5712328a654cd96cd5f29
Bhupendra2001 <bhupendran42@gmail.com>
2023-03-08 10:51:55 +0530

    done

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2')
 
 
 const db = mysql.createPool({
-    host : "http://blog-website-three-gamma.vercel.app",
+    host : "localhost",
     user : "root",
     password : "Sahil@123",
     database : "blog"
--- api/routes/router.js
+++ api/routes/router.js
@@ -2,9 +2,15 @@ const express = require('express')
 const {register,login, logout} = require('../controllers/auth')
 const { addPost,getPost, getPosts, updatePost,deletePost} = require('../controllers/post')
 const router = express.Router()
+const {db} = require('../db')
 
 router.get('/', (req,res)=>{
-    
+    const q = "select * from user"
+
+    db.query(q, (err, data) => {
+        if (err) return res.status(400).send(err);
+        return res.status(200).send(data);
+      });
 })
 
 
